2652547ff296afbd1ee3e73641534e5693a340c2
Lakshan-Dananjana <lakshandananjana253@gmail.com>
2025-05-17 23:56:27 +0530

    Sixth Commit

--- src/Components/SkillsBody.jsx
+++ src/Components/SkillsBody.jsx
@@ -5,7 +5,7 @@ function SkillsBody() {
     <div className='w-[100%] h-[100%]'>
       <h1 className='text-center font-bold text-3xl pt-[25px] '>Skills</h1>
         <div className='w-[100%] h-[90%] flex justify-center items-center gap-[50px] '>
-            <div className='w-[30%] h-[60%] border-[1px] rounded-xl flex p-[15px] items-center flex-col shadow-2xl shadow-cyan-400 '>
+            <div className='xl:w-[30%] w-[45%] h-[60%] border-[1px] rounded-xl flex p-[15px] items-center flex-col shadow-2xl shadow-cyan-400 '>
                 <h2 className='text-2xl font-bold'>Soft Skills</h2>
                 <ul className='w-[80%] h-[60%] pt-[25px] flex flex-col gap-[15px] justify-center '>
                   <li>Teamwork & Collaboration</li>
@@ -14,7 +14,7 @@ function SkillsBody() {
                   <li>Service-Oriented Mindset</li>
                 </ul>
             </div>
-            <div className='w-[30%] h-[60%] border-[1px] rounded-xl flex p-[10px] items-center flex-col shadow-2xl shadow-cyan-400 '>
+            <div className='xl:w-[30%] w-[45%] h-[60%] border-[1px] rounded-xl flex p-[10px] items-center flex-col shadow-2xl shadow-cyan-400 '>
                 <h2 className='text-xl font-bold'>Technical Skills</h2>
                 <ul className='w-[80%] h-[90%] pt-[25px] flex flex-col gap-[15px] justify-center '>
                   <li>PHP, HTML, CSS, JavaScript</li>
